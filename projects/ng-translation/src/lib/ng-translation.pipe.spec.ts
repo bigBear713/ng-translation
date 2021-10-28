9a7493560cb3e4453a32d2cf7b57c1788ac5b8c2
bigBear713 <774326739@qq.com>
2021-10-28 23:39:14 +0800

    ADD: ngTranslation pipe

---
+++ projects/ng-translation/src/lib/ng-translation.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NgTranslationPipe } from './ng-translation.pipe';
+
+describe('Pipe: NgTranslatione', () => {
+  it('create an instance', () => {
+    let pipe = new NgTranslationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
