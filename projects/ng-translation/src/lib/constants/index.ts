b0477f54e9147cb0a7f5802c515b8aee6f9eb541
bigBear713 <774326739@qq.com>
2022-01-16 21:31:35 +0800

    UPDATE: return trans key default when trans content is empty

--- projects/ng-translation/src/lib/constants/index.ts
+++ projects/ng-translation/src/lib/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './ng-translation-loader.token';
 export * from './ng-translation-default-lang.token';
+export * from './ng-translation-max-retry.token';
---
+++ projects/ng-translation/src/lib/constants/ng-translation-max-retry.token.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from "@angular/core";
+
+export const NG_TRANS_MAX_RETRY_TOKEN = new InjectionToken<number>('ng-translation-max-retry');
