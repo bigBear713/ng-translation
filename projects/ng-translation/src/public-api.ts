8b90ee53b5cf16ff3766b597537118db68fba020
wjx774326739 <774326739@qq.com>
2022-02-26 19:02:21 +0800

    UPDATE: export file

--- projects/ng-translation/src/public-api.ts
+++ projects/ng-translation/src/public-api.ts
@@ -6,5 +6,6 @@ export * from './lib/services';
 export * from './lib/components';
 export * from './lib/pipes/ng-trans.pipe';
 export * from './lib/ng-trans.module';
+export * from './lib/ng-trans-testing.module';
 export * from './lib/constants';
 export * from './lib/models';
