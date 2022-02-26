b804b801b5847902347ece3af988aa1cadb2c87f
bigBear713 <774326739@qq.com>
2022-02-26 19:02:21 +0800

    UPDATE: export file

--- projects/ng-translation/src/lib/models/index.ts
+++ projects/ng-translation/src/lib/models/index.ts
@@ -3,3 +3,5 @@ export * from './ng-trans-lang.enum';
 export * from './ng-trans-params.interface';
 export * from './ng-trans-options.interface';
 export * from './ng-trans-change-lang.interface';
+export * from './ng-trans-sentence-item.enum';
+export * from './ng-trans-sentence-part.interface';
